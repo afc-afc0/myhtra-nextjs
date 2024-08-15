a3bf3626deac470fdd33c8d374e0c2a1a9f68056
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-08-15 15:05:40 -0400

    Accordion added

--- components/ui/Accordion/Accordion.tsx
+++ components/ui/Accordion/Accordion.tsx
@@ -1,16 +1,69 @@
+'use client'
+import React, { ComponentProps, ComponentPropsWithoutRef } from 'react'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import { ChevronRightSVG } from '../SVG/SVG'
+
+import styles from './Accordion.module.css'
+import clsx from 'clsx'
+import { FlexContainer } from '../Layout/FlexContainer/FlexContainer'
 
 
-interface AccordionProps {
-  children: React.ReactNode,
-  title: string,
-  isOpen?: boolean,
-  onToggle?: () => void
-}
+export const Accordion = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AccordionPrimitive.Root
+    ref={ref}
+    className={clsx(styles.accordion, className)}
+    {...props}
+  />
+))
+Accordion.displayName = 'Accordion'
 
-export const Accordion = ({ children, title, isOpen = false, onToggle } : AccordionProps) => {
-  return (
-    <>
+export const AccordionItem = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ ...props }, ref) => (
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={styles.item}
+    {...props}
+  />
+))
+AccordionItem.displayName = 'AccordionItem'
 
-    </>
-  )
-}
+export const AccordionTrigger = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Header>
+    <AccordionPrimitive.Trigger
+      ref={ref}
+      className={styles.trigger}
+      {...props}
+    >
+      <FlexContainer height='100%' paddingSize='s' alignItems='center' flexDirection='row' justifyContent='space-between'>
+        {children}
+        <ChevronRightSVG className={styles.triggerIcon} />
+      </FlexContainer>
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+))
+AccordionTrigger.displayName = 'AccordionTrigger'
+
+export const AccordionContent = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Content
+    ref={ref}
+    className={styles.content}
+    {...props}
+  >
+    <FlexContainer paddingSize='s'>
+      {children}
+    </FlexContainer>
+  </AccordionPrimitive.Content>
+))
+AccordionContent.displayName = 'AccordionContent'
+
