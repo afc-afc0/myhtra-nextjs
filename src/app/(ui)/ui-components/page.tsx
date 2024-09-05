e96eb052bcf44051d4c154ce7ca151107e059512
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-04 23:12:00 -0400

    ui components page layout imptovements

--- src/app/(ui)/ui-components/page.tsx
+++ src/app/(ui)/ui-components/page.tsx
@@ -5,7 +5,6 @@ import { FlexContainer } from '@components/ui/Layout/FlexContainer/FlexContainer
 import { PageContainer } from '@components/ui/PageContainer/PageContainer'
 import { ThemeSwitch } from '@components/ui/ThemeSwitch/ThemeSwitch'
 import { useState } from 'react'
-import { LayoutContentContainer } from '@components/ui/Layout/LayoutContentContainer/LayoutContentContainer'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@components/ui/Form/Select/Select'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@components/ui/Accordion/Accordion'
 import { LexicalRichText } from '@components/ui/Lexical/react-rich/src/LexicalRichText'
@@ -14,63 +13,76 @@ import styles from './page.module.css'
 
 export default function Home() {
   const [inputTextValue, setInputTextValue] = useState<string>('')
+  const [selectValue, setSelectValue] = useState<string>('')
 
   return (
-    <PageContainer>
-      <LayoutContentContainer>
-        <Container>
-          <FlexContainer>
-            <Label label='Input text' htmlFor='inputText' />
-            <Input id='inputText' value={inputTextValue} onChange={e => setInputTextValue(e.target.value)} /> 
+    <FlexContainer width='100%' alignItems='center' paddingSize='l'>
+      <Container> 
+        <FlexContainer width='inherit' height='auto' borderRadius='m' borderWidth='m' paddingSize='s' gapSize='s'>
+        <ThemeSwitch /> 
+          <FlexContainer width='100%' flexDirection='row' flexGrow={1} flexBasis='%0' gapSize='s'>
+            <FlexContainer width='50%'>
+              <Label label='Input text' htmlFor='inputText' />
+              <Input id='inputText' value={inputTextValue} onChange={e => setInputTextValue(e.target.value)} /> 
+            </FlexContainer>
+            <FlexContainer width='50%'>
+              <Label label='Select Component' htmlFor='select' />
+              <Select onValueChange={value => setSelectValue(value)}>
+                <SelectTrigger>
+                  <SelectValue>{selectValue}</SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='1'>Option 1</SelectItem>
+                  <SelectItem value='2'>Option 2</SelectItem>
+                  <SelectItem value='3'>Option 3</SelectItem>
+                </SelectContent>
+              </Select>
+            </FlexContainer>
           </FlexContainer>
-          <FlexContainer>
-            <Label label='Select Component' htmlFor='select' />
-            <Select>
-              <SelectTrigger>
-                <SelectValue>Select</SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value='1'>Option 1</SelectItem>
-                <SelectItem value='2'>Option 2</SelectItem>
-                <SelectItem value='3'>Option 3</SelectItem>
-              </SelectContent>
-            </Select>
+          <FlexContainer width='100%' height='100%'>
+            <Accordion type='single' collapsible> 
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-2">
+                <AccordionTrigger>Is it styled?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It comes with default styles that matches the other
+                  components&apos; aesthetic.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-3">
+                <AccordionTrigger>Is it animated?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It&apos;s animated by default, but you can disable it if you
+                  prefer.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </FlexContainer>
-          <ThemeSwitch />
-        </Container>
-      </LayoutContentContainer>
-      <FlexContainer width='100%' height='100%'>
-        <Accordion type='single' collapsible> 
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Is it animated?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It&apos;s animated by default, but you can disable it if you
-              prefer.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <LexicalRichText />
-      </FlexContainer>
-    </PageContainer>
+          <FlexContainer width='100%' height='auto'>
+            <LexicalRichText />
+          </FlexContainer>
+        </FlexContainer>
+      </Container>
+    </FlexContainer>
   )
 }
 
 const Container = ({ children } : { children: React.ReactNode }) => {
   return (
     <div className={styles.container}>
+      { children }
+    </div>
+  )
+}
+
+const TwoColumnContainer = ({ children } : { children: React.ReactNode }) => {
+  return (
+    <div className={styles.twoColumnContainer}>
       { children }
     </div>
   )
--- src/components/ui/Layout/FlexContainer/FlexContainer.tsx
+++ src/components/ui/Layout/FlexContainer/FlexContainer.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import clsx from 'clsx'
-
 import styles from './FlexContainer.module.css'
 
 interface FlexContainerProps {
@@ -10,58 +9,73 @@ interface FlexContainerProps {
   // Layout
   flexDirection?: 'row' | 'column',
   responsiveFlexDirection?: 'none' | 'row' | 'column',
-  paddingSize?: 'none' | 'xs' | 's' | 'm' | 'l' | 'xl',  
+  paddingSize?: 'none' | 'xs' | 's' | 'm' | 'l' | 'xl',
   gapSize?: 'none' | 'xs'| 's' | 'm' | 'l',
   marginBottom?: 'none' | 'xs' | 's' | 'm' | 'l' | 'xl',
-  height?: 'none' | '100%' | 'auto' | 'inherit',
-  width?: 'none' | '100%' | 'auto' | 'inherit',
+  height?: 'none' | '25%' | '50%' | '33%' | '100%' | 'auto' | 'inherit',
+  width?: 'none' | '25%' | '50%' | '33%' | '100%' | 'auto' | 'inherit',
   justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around',
   alignItems?: 'flex-start' | 'flex-end' | 'center',
-  style?: React.CSSProperties,  
+  flexBasis?: 'none' | '%0' | '%25' | '%50' | '%75' | '%100',
+  flexGrow?: 1,
+  flexShrink?: 1,
+  boxSizing?: 'content-box' | 'border-box',
+  style?: React.CSSProperties,
   // Effects
   borderWidth?: 'none' | 'xs' | 's' | 'm' | 'l' | 'xl',
   borderRadius?: 'none' | 'xs' | 's' | 'm' | 'l' | 'xl',
+  borderTopRadius?: 'none' | 'xs' | 's' | 'm' | 'l' | 'xl',
   shadowSize?: 'none' | 's' | 'm' | 'l',
 }
 
-export const FlexContainer = React.forwardRef<HTMLDivElement, FlexContainerProps>(({ 
-  children, 
+export const FlexContainer = React.forwardRef<HTMLDivElement, FlexContainerProps>(({
+  children,
   className,
   id,
   flexDirection = 'column',
-  responsiveFlexDirection = 'none', 
-  paddingSize = 'none', 
-  gapSize = 'none', 
+  responsiveFlexDirection = 'none',
+  paddingSize = 'none',
+  gapSize = 'none',
   marginBottom = 'none',
   height = 'auto',
   width = 'auto',
   justifyContent = 'flex-start',
   alignItems = 'flex-start',
+  flexBasis = 'none',
+  flexGrow = 'none',
+  flexShrink = 'none',
+  boxSizing = 'none',
   borderWidth = 'none',
   borderRadius = 'none',
+  borderTopRadius = 'none',
   shadowSize = 'none',
   style,
 }, ref) => {
   return (
-    <div 
+    <div
       ref={ref}
       className={clsx(styles.flexContainer, className)}
-      id={id} 
-      data-padding-size={paddingSize} 
-      data-gap-size={gapSize} 
+      id={id}
+      data-padding-size={paddingSize}
+      data-gap-size={gapSize}
       data-margin-bottom={marginBottom}
       data-flex-direction={flexDirection}
       data-height={height}
       data-width={width}
       data-justify-content={justifyContent}
       data-align-items={alignItems}
-      data-border-width={borderWidth} 
-      data-border-radius={borderRadius} 
+      data-flex-basis={flexBasis}
+      data-flex-grow={flexGrow}
+      data-flex-shrink={flexShrink}
+      data-border-width={borderWidth}
+      data-border-radius={borderRadius}
+      data-border-top-radius={borderTopRadius}
       data-shadow-size={shadowSize}
+      data-box-sizing={boxSizing}
       data-responsive-flex-direction={responsiveFlexDirection}
       style={style}
     >
-      { children }
+      {children}
     </div>
   )
 })
