b75b349456c5562cb098b7f96026e60a8483a8e9
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-15 19:21:39 -0400

    Input size updates

--- src/components/ui/Form/Input/Input.tsx
+++ src/components/ui/Form/Input/Input.tsx
@@ -1,15 +1,32 @@
+import React, { forwardRef } from 'react'
 import sharedStyles from '../Shared/Shared.module.css'
 import styles from './Input.module.css'
+import clsx from 'clsx'
 
-interface InputTextProps {
-  value: string,
-  id: string,
-  autoComplete?: string,
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+interface InputTextProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  value: string
+  id?: string
+  autoComplete?: string
+  className?: string
+  size?: 'xs' | 's' | 'm' | 'l' | 'xl'
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({ id, value, autoComplete = 'off', onChange }: InputTextProps) => {
-  return (
-    <input id={id} className={`${styles.input}  ${sharedStyles.shared}`} autoComplete={autoComplete} value={value} onChange={onChange} />
-  )
-}
+export const Input = forwardRef<HTMLInputElement, InputTextProps>(
+  ({ id, value, className, size, autoComplete = 'off', onChange, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        id={id}
+        className={clsx(styles.input, sharedStyles.shared, className)}
+        autoComplete={autoComplete}
+        value={value}
+        onChange={onChange}
+        data-size={size}
+        {...props}
+      />
+    )
+  }
+)
+
+Input.displayName = 'Input'
