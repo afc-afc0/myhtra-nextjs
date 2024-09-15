d252d9696e2c3b8031be9c478981281b95f74d98
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-15 19:23:28 -0400

    auth changes

---
+++ src/app/api/auth/logout/route.js
@@ -0,0 +1,20 @@
+import { authOptions } from '@utils/nextAuth/authOptions'
+import { getIdToken } from '@utils/nextAuth/sessionTokenAccessor'
+import { getServerSession } from "next-auth"
+
+export async function GET() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    const idToken = await getIdToken()
+    var url = `${process.env.KEYCLOAK_END_SESSION_URL}?id_token_hint=${idToken}&post_logout_redirect_uri=${encodeURIComponent(process.env.NEXTAUTH_URL)}`
+
+    try {
+      const resp = await fetch(url, { method: "GET" })
+    } catch (err) {
+      console.error(err)
+      return new Response({ status: 500 })
+    }
+  }
+  return new Response({ status: 200 })
+}
