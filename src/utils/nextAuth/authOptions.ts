433c1247cf807a1b4a25304fb1b23a1e43fab228
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-19 23:47:09 -0400

    NextAuth updated with audience giving clientside token and scope updates

--- src/utils/nextAuth/authOptions.ts
+++ src/utils/nextAuth/authOptions.ts
@@ -1,6 +1,23 @@
 import KeyCloakProvider from "next-auth/providers/keycloak";
 import { encrypt } from '@utils/nextAuth/encryption'
 import { jwtDecode } from "jwt-decode";
+import { DefaultSession, Session } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    access_token: { iv: string; encryptedData: string }
+    client_access_token: string
+    id_token: { iv: string; encryptedData: string }
+    roles: string[]
+    resource_roles: string[]
+    user: {
+      given_name: string
+      family_name: string
+      id: string
+    } & DefaultSession["user"]
+    error?: string
+  }
+}
 
 const refreshAccessToken = async (token: { refresh_token: any; }) => {
   const resp = await fetch(`${process.env.KEYCLOAK_REFRESH_TOKEN_URL}`, {
@@ -10,6 +27,7 @@ const refreshAccessToken = async (token: { refresh_token: any; }) => {
       client_secret: process.env.KEYCLOAK_CLIENT_SECRET || '',
       grant_type: "refresh_token",
       refresh_token: token.refresh_token,
+      audience: 'mythra-net-backend-audience'
     }),
     method: "POST",
   })
@@ -31,45 +49,55 @@ export const authOptions  = {
       clientId: `${process.env.KEYCLOAK_CLIENT_ID}`,
       clientSecret: `${process.env.KEYCLOAK_CLIENT_SECRET}`,
       issuer: `${ process.env.KEYCLOAK_REALM_URL}`,
+      authorization: { params: { scope: "openid email profile mythra-net-backend-scope", audience: 'mythra-net-backend-aud' } },
+      token: { params: { audience: 'mythra-net-backend-aud' } },
     })
   ],
   callbacks: {
-      async jwt({ token, account }: { token: any, account: any }) {
-        const nowTimeStamp = Math.floor(Date.now() / 1000)
+    async jwt({ token, account }: { token: any, account: any }) {
+      const nowTimeStamp = Math.floor(Date.now() / 1000)
 
-        // First time access token is returned
-        if (account) {
-          token.decoded = jwtDecode(account.access_token)
-          token.access_token = account.access_token
-          token.id_token = account.id_token
-          token.expires_at = account.expires_at
-          token.refresh_token = account.refresh_token
-          return token
-        }
-
-        // Check the timestamp of the access token to see if it's expired
-        // if it not return the token otherwise refresh the token
-        if (nowTimeStamp < token.expires_at) {
-          return token
-        } else {
-          try {
-            const refreshedToken = await refreshAccessToken(token)
-            return refreshedToken;
-          } catch (error) {
-            return { ...token, error: "RefreshAccessTokenError" }
-          }
-        }
-      },
-      async session({ session, token }: { session: any, token: any }) {
-        session.access_token = encrypt(token.access_token) 
-        session.id_token = encrypt(token.id_token)
-        session.roles = token.decoded.realm_access.roles
-        session.resource_roles = token.decoded.resource_access?.['myhtra-frontend']?.roles || []
-        session.user.given_name = token.decoded.given_name
-        session.user.family_name = token.decoded.family_name
-        session.user.id = token.decoded.sub
-        session.error = token.error
-        return session
+      // First time access token is returned
+      if (account) {
+        token.decoded = jwtDecode(account.access_token)
+        token.access_token = account.access_token
+        token.id_token = account.id_token
+        token.expires_at = account.expires_at
+        token.refresh_token = account.refresh_token
+        return token
       }
+
+      // Check the timestamp of the access token to see if it's expired
+      // if it not return the token otherwise refresh the token
+      if (nowTimeStamp < token.expires_at) {
+        return token
+      } else {
+        try {
+          const refreshedToken = await refreshAccessToken(token)
+          return refreshedToken;
+        } catch (error) {
+          return { ...token, error: "RefreshAccessTokenError" }
+        }
+      }
+    },
+    async session({ session, token }: { session: Session, token: any }) {
+      if (token.error === "RefreshAccessTokenError") {
+        console.log("RefreshAccessTokenError")
+        return {
+          expires: session.expires,
+          error: "RefreshAccessTokenError"
+        }
+      }
+
+      session.access_token = encrypt(token.access_token)
+      session.client_access_token = token.access_token
+      session.id_token = encrypt(token.id_token)
+      session.roles = token.decoded.realm_access.roles
+      session.resource_roles = token.decoded.resource_access?.['myhtra-frontend']?.roles || []
+      session.user.given_name = token.decoded.given_name
+      session.user.family_name = token.decoded.family_name
+      session.user.id = token.decoded.sub
+      return session
+    }
   }
 }
