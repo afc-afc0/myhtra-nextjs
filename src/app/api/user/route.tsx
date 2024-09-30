4f4f0f68519e0f09db0706b0e1638e8779b6a20b
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-29 22:11:16 -0400

    UserInfo api route added

---
+++ src/app/api/user/route.tsx
@@ -0,0 +1,27 @@
+import { getAccessToken } from "@utils/nextAuth/getAccessToken";
+
+const keycloakUrl = process.env.KEYCLOAK_REALM_URL
+
+export async function GET(req: any) {
+  try {
+    const accessToken = await getAccessToken({ req })
+
+    const res = await fetch(`${keycloakUrl}/protocol/openid-connect/userinfo`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method: "GET",
+    })
+
+    if (!res.ok) {
+      console.log("Failed to fetch error: ", res)
+      throw new Error("Failed to fetch error: ")
+    }
+
+    let data = await res.json()
+    return Response.json(data, { status: res.status })
+  } catch (error) {
+    return Response.json({ error: "Internal Server Error" }, { status: 500 })
+  }
+}
