6a21fa51a90689e5d94bfe830511d0f810059347
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-30 12:34:45 -0400

    BlogPosts added

---
+++ src/app/(default)/_components/BlogPosts/BlogPosts.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { FlexContainer } from '@components/ui/Layout/FlexContainer/FlexContainer'
+import { Posts } from '@components/ui/Posts/Posts'
+import { Footer } from '@components/ui/Footer/Footer'
+
+import styles from './BlogPosts.module.css'
+
+export const BlogPosts = () => {
+  return (
+    <FlexContainer id='blogPosts' height='100%' alignItems='center' justifyContent='space-between' flexDirection='column'>
+      <FlexContainer width='100%' flexGrow={1} justifyContent='center' alignItems='center'>
+        <Container>
+          <FlexContainer className={styles.overflow} paddingSize='s' borderWidth='l' borderRadius='m' height='inherit' width='inherit' flexDirection='column' gapSize='s' >
+            <Posts />
+          </FlexContainer>
+        </Container>
+      </FlexContainer>
+      <FlexContainer justifyContent='center' alignItems='center'>
+        <Footer />
+      </FlexContainer>     
+    </FlexContainer>
+  )
+}
+
+const Container = ({ children } : { children: React.ReactNode }) => {
+  return (
+    <div className={styles.container}>
+      { children }
+    </div>
+  )
+}
