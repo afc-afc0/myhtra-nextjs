75cb14b7b8f53bdfb120bb188968a01d2f26b417
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-29 22:14:17 -0400

    Deleted unused funcstions in getAccessToken

--- src/utils/nextAuth/getAccessToken.tsx
+++ src/utils/nextAuth/getAccessToken.tsx
@@ -12,34 +12,3 @@ export const getAccessToken = async ({ req } : { req: any }) => {
 
   return access_token
 }
-
-// export const getAccessToken = async ({ accessToken } : { accessToken: any }) => {
-//   const session = await getServerSession(authOptions) as Session | null
-
-//   if(accessToken){    
-//     const accessTokenDecrypted = decrypt(accessToken)    
-//     return accessTokenDecrypted
-//   }
-
-//   return null
-// }
-
-// export const getAccessTokenTest = async () => {
-//   const session = await getServerSession(authOptions) as Session | null
-
-//   if(session){    
-//     const accessTokenDecrypted = decrypt(session.access_token)    
-//     return accessTokenDecrypted
-//   }
-
-//   return null
-// }
-
-// export const getIdToken = ({ idToken } : { idToken: any }) => {
-//   if(idToken){    
-//     const idTokenDecrypted = decrypt(idToken.id_token)    
-//     return idTokenDecrypted
-//   }
-
-//   return null
-// }
