f887096e0eaf6e583e5a7ea09d4313ba0afce95c
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-30 12:37:14 -0400

    Theme switch updated

--- src/components/ui/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ui/ThemeSwitch/ThemeSwitch.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
+import { Popover, PopoverContent, PopoverTrigger } from '@components/ui/Popover/Popover'
+import { Toggle } from '@components/ui/Toggle/Toggle'
+import { DarkModeSVG } from '@components/ui/SVG/SVG'
+import { Button } from '@components/ui/Button/Button'
+import { FlexContainer } from '../Layout/FlexContainer/FlexContainer'
 
 export function ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
   const { theme, setTheme } = useTheme()
 
   useEffect(() => {
@@ -14,10 +22,41 @@ export function ThemeSwitch() {
   }
 
   return (
-    <select value={theme} onChange={e => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+      <PopoverTrigger asChild>
+        <Toggle 
+          isPressed={isPopoverOpen}
+          icon={<DarkModeSVG />}
+          size='m'
+          aria-label='Theme'
+        />
+      </PopoverTrigger>
+      <PopoverContent>
+        <FlexContainer flexDirection='column' gapSize='s'>
+          <Button
+            text='System'
+            onClick={() => setTheme('system')}
+            size='m'
+          />
+          <Button
+            text='Dark'
+            onClick={() => setTheme('dark')}
+            size='m'
+          />
+          <Button
+            text='Light'
+            onClick={() => setTheme('light')}
+            size='m'
+          />
+        </FlexContainer>
+      </PopoverContent>
+    </Popover>
+
+
+    // <select value={theme} onChange={e => setTheme(e.target.value)}>
+    //   <option value="system">System</option>
+    //   <option value="dark">Dark</option>
+    //   <option value="light">Light</option>
+    // </select>
   )
 }
