95666914f829bbb386541ad24161490f2542937b
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-29 22:08:47 -0400

    GET and POST api routes for Posts

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,70 @@
+export const dynamic = (request: Request) => {
+  return request.method === 'GET' ? 'force-static' : 'force-dynamic';
+}
+
+import { getAccessToken } from "@utils/nextAuth/getAccessToken";
+import fetch from 'node-fetch'
+import https from 'https'
+
+export const agent = process.env.NODE_ENV === 'development'
+  ? new https.Agent({ rejectUnauthorized: false })
+  : https.globalAgent
+
+const api = process.env.NEXT_PUBLIC_MYHTRA_API
+
+export async function GET(req: any) {
+  try {
+    const accessToken = await getAccessToken({ req })
+
+    const res = await fetch(`${api}/Post`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method: "GET",
+      agent: agent
+    })
+
+    if (!res.ok) {
+      const error = await res.text() 
+      throw new Error("Failed to fetch error: " + error)
+    }
+    
+    const data = await res.json()
+    return Response.json(data, { status: res.status })
+  }
+  catch (error) {
+    console.error(error)
+    return Response.json({ error: "Internal Server Error" }, { status: 500 })
+  }
+}
+
+export async function POST(req: any) {
+  try {
+    const accessToken = await getAccessToken({ req })
+    
+    const jsonBody = await req.json()
+
+    const res = await fetch(`${api}/Post`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method: "POST",
+      body: JSON.stringify(jsonBody),
+      agent: agent
+    })
+    
+    if (!res.ok) {
+      const error = await res.text()
+      throw new Error("Failed to fetch error: " + error)
+    }
+
+    const data = await res.json()
+    return Response.json(data, { status: res.status })
+  }
+  catch (error) {
+    console.error(error)
+    return Response.json({ error: "Internal Server Error" }, { status: 500 })
+  }
+}
