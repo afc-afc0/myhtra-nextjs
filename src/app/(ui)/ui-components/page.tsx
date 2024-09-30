4293573594a3dc390026de623ada330bf12b60fb
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-09-29 22:11:02 -0400

    Dialog + Checkbox added to ui components

--- src/app/(ui)/ui-components/page.tsx
+++ src/app/(ui)/ui-components/page.tsx
@@ -15,122 +15,150 @@ import { ChevronRightSVG } from '@components/ui/SVG/SVG'
 import { Text } from '@components/ui/Text/Text'
 import { Popover, PopoverContent, PopoverTrigger } from '@components/ui/Popover/Popover'
 import { AuthController } from '@components/ui/Auth/AuthController/AuthController'
-import Logo from '@public/svgs/Logo.svg'
+import { DialogHeader, DialogContent, Dialog, DialogTrigger, DialogFooter } from '@components/ui/Dialog/Dialog'
 
 import styles from './page.module.css'
-import Image from 'next/image'
+import { PageContainer } from '@components/ui/PageContainer/PageContainer'
+import { Checkbox } from '@components/ui/Form/Checkbox/Checkbox'
 
 export default function Home() {
   const [inputTextValue, setInputTextValue] = useState<string>('')
   const [selectValue, setSelectValue] = useState<string>('')
-  const { lexicalContent, canSubmit, onChange } = useLexical()
 
   return (
-    <FlexContainer width='100%' alignItems='center' paddingSize='l'>
-      <Container> 
-        <FlexContainer width='inherit' height='auto' borderRadius='m' borderWidth='m' paddingSize='s' gapSize='s'>          
-          <FlexContainer width='100%' flexDirection='row' flexGrow={1} flexBasis='%0' gapSize='s'>
+    <PageContainer>
+      <FlexContainer width='100%' alignItems='center' paddingSize='l'>
+        <Container> 
+          <FlexContainer width='inherit' height='auto' borderRadius='m' borderWidth='m' paddingSize='s' gapSize='s'>          
+            <FlexContainer width='100%' flexDirection='row' flexGrow={1} flexBasis='%0' gapSize='s'>
+              <FlexContainer width='50%'>
+                <Label label='Input text' htmlFor='inputText' />
+                <Input id='inputText' value={inputTextValue} onChange={e => setInputTextValue(e.target.value)} /> 
+              </FlexContainer>
+            </FlexContainer>
+            <FlexContainer width='100%' height='100%'>
+              <Accordion type='single' collapsible> 
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>Is it styled?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It comes with default styles that matches the other
+                    components&apos; aesthetic.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                  <AccordionTrigger>Is it animated?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It&apos;s animated by default, but you can disable it if you
+                    prefer.
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </FlexContainer>
+            <ThemeSwitch />          
+            <FlexContainer width='100%' height='auto'>
+              <LexicalRichText />
+            </FlexContainer>
+            <FlexContainer width='100%' height='auto'>
+              <Button
+                text='Button'
+              />
+            </FlexContainer>
+            <FlexContainer width='100%' height='auto' gapSize='s'>
+              <Text text='Dialog' />
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button text='Open Dialog' />
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <Text text='Dialog Header' />
+                  </DialogHeader>
+                  <FlexContainer width='100%' gapSize='s'>
+                    <Text text='Dialog Content' />
+                    <Text text='Dialog Content' />
+                    <Text text='Dialog Content' />
+                    <Text text='Dialog Content' />
+                  </FlexContainer>
+                </DialogContent>
+                <DialogFooter>
+                  <Button text='Submit'>Save changes</Button>
+                </DialogFooter>
+              </Dialog>
+            </FlexContainer>
+            <FlexContainer width='100%' height='auto'>
+              <Text text='Droddown Menu currently in testing' />
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button
+                    icon={<ChevronRightSVG />}
+                  />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    Account
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <Text text='Text' />
+                    </DropdownMenuItem>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <Text text='Sub Text' />
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuItem>
+                        <Text text='Text' />
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <Text text='Text' />
+                      </DropdownMenuItem>
+                    </DropdownMenuSub>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </FlexContainer>
             <FlexContainer width='50%'>
-              <Label label='Input text' htmlFor='inputText' />
-              <Input id='inputText' value={inputTextValue} onChange={e => setInputTextValue(e.target.value)} /> 
+              <Label label='Select Component' htmlFor='select' />
+              <Select onValueChange={value => setSelectValue(value)}>
+                <SelectTrigger>
+                  <SelectValue>{selectValue}</SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='1'>Option 1</SelectItem>
+                  <SelectItem value='2'>Option 2</SelectItem>
+                  <SelectItem value='3'>Option 3</SelectItem>
+                </SelectContent>
+              </Select>
+            </FlexContainer>
+            <FlexContainer width='100%' height='auto'>
+              <Text text='Popover' />
+              <Popover>
+                <PopoverTrigger>
+                  <Button text='Click me' />
+                </PopoverTrigger>
+                <PopoverContent>
+
+                </PopoverContent>
+              </Popover>
+            </FlexContainer>
+            <FlexContainer width='100%' height='auto'>
+              <Text text='Auth Controller' />
+              <AuthController />
+            </FlexContainer>
+            <FlexContainer width='100%' height='auto'>
+              <Text text='Checkbox' />
+              <Checkbox size='xs' />
             </FlexContainer>
           </FlexContainer>
-          <FlexContainer width='100%' height='100%'>
-            <Accordion type='single' collapsible> 
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-2">
-                <AccordionTrigger>Is it styled?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It comes with default styles that matches the other
-                  components&apos; aesthetic.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-3">
-                <AccordionTrigger>Is it animated?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It&apos;s animated by default, but you can disable it if you
-                  prefer.
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </FlexContainer>
-          <ThemeSwitch />          
-          <FlexContainer width='100%' height='auto'>
-            <LexicalRichText onChange={onChange} />
-          </FlexContainer>
-          <FlexContainer width='100%' height='auto'>
-            <Button
-              text='Button'
-            />
-          </FlexContainer>
-          <FlexContainer width='100%' height='auto'>
-            <Text text='Droddown Menu currently in testing' />
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Button
-                  icon={<ChevronRightSVG />}
-                />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>
-                  Account
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <Text text='Text' />
-                  </DropdownMenuItem>
-                  <DropdownMenuSub>
-                    <DropdownMenuSubTrigger>
-                      <Text text='Sub Text' />
-                    </DropdownMenuSubTrigger>
-                    <DropdownMenuItem>
-                      <Text text='Text' />
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Text text='Text' />
-                    </DropdownMenuItem>
-                  </DropdownMenuSub>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </FlexContainer>
-          <FlexContainer width='50%'>
-            <Label label='Select Component' htmlFor='select' />
-            <Select onValueChange={value => setSelectValue(value)}>
-              <SelectTrigger>
-                <SelectValue>{selectValue}</SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value='1'>Option 1</SelectItem>
-                <SelectItem value='2'>Option 2</SelectItem>
-                <SelectItem value='3'>Option 3</SelectItem>
-              </SelectContent>
-            </Select>
-          </FlexContainer>
-          <FlexContainer width='100%' height='auto'>
-            <Text text='Popover' />
-            <Popover>
-              <PopoverTrigger>
-                <Button text='Click me' />
-              </PopoverTrigger>
-              <PopoverContent>
-
-              </PopoverContent>
-            </Popover>
-          </FlexContainer>
-          <FlexContainer width='100%' height='auto'>
-            <Text text='Auth Controller' />
-            <AuthController />
-          </FlexContainer>
-        </FlexContainer>
-      </Container>
-    </FlexContainer>
+        </Container>
+      </FlexContainer>
+    </PageContainer>
   )
 }
 
