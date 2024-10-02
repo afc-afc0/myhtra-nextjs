131b36d4546414ed2b46c5dde7e0854d145e673a
afc-afc0 <ahmet_fatih_cengiz@hotmail.com>
2024-10-02 09:46:54 -0400

    Put CreatePost button in BlogPosts

--- src/app/(default)/_components/BlogPosts/BlogPosts.tsx
+++ src/app/(default)/_components/BlogPosts/BlogPosts.tsx
@@ -3,6 +3,7 @@
 import { FlexContainer } from '@components/ui/Layout/FlexContainer/FlexContainer'
 import { Posts } from '@components/ui/Posts/Posts'
 import { Footer } from '@components/ui/Footer/Footer'
+import { CreatePostDialog } from '@components/ui/Posts/CreatePostDialog/CreatePostDialog'
 
 import styles from './BlogPosts.module.css'
 
@@ -11,8 +12,11 @@ export const BlogPosts = () => {
     <FlexContainer id='blogPosts' height='100%' alignItems='center' justifyContent='space-between' flexDirection='column'>
       <FlexContainer width='100%' flexGrow={1} justifyContent='center' alignItems='center'>
         <Container>
-          <FlexContainer className={styles.overflow} paddingSize='s' borderWidth='l' borderRadius='m' height='inherit' width='inherit' flexDirection='column' gapSize='s' >
+          <FlexContainer className={styles.overflow} paddingSize='s' borderWidth='l' borderRadius='m' height='inherit' width='inherit' flexDirection='column' gapSize='s' marginBottom='s' >
             <Posts />
+          </FlexContainer>
+          <FlexContainer justifyContent='flex-end' alignItems='flex-end'>
+            <CreatePostDialog />
           </FlexContainer>
         </Container>
       </FlexContainer>
