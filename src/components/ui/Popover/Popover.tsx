80c716234e9ce2dbf282b770b769a938ff9ecc83
Ahmet Cengiz <ahmet_fatih_cengiz@hotmail.com>
2025-06-18 00:35:47 -0400

    Refactor Popover component for consistent quotation style and improve type definition

--- src/components/ui/Popover/Popover.tsx
+++ src/components/ui/Popover/Popover.tsx
@@ -1,8 +1,8 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import * as PopoverPrimitive from "@radix-ui/react-popover"
-import clsx from "clsx"
+import * as React from 'react'
+import * as PopoverPrimitive from '@radix-ui/react-popover'
+import clsx from 'clsx'
 
 import styles from './Popover.module.css'
 
@@ -11,21 +11,12 @@ export const Popover = PopoverPrimitive.Root
 export const PopoverTrigger = PopoverPrimitive.Trigger
 
 export const PopoverContent = React.forwardRef<
-  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => {
+>(({ className, align = 'center', sideOffset = 4, ...props }, ref) => {
   return (
     <PopoverPrimitive.Portal>
-      <PopoverPrimitive.Content
-        ref={ref}
-        align={align}
-        sideOffset={sideOffset}
-        className={clsx(
-          styles.content,
-          className
-        )}
-        {...props}
-      />
+      <PopoverPrimitive.Content ref={ref} align={align} sideOffset={sideOffset} className={clsx(styles.content, className)} {...props} />
     </PopoverPrimitive.Portal>
   )
 })
